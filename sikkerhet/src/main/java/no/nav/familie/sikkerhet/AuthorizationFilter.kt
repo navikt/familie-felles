28b439f81d17407e5dd092f699ae438fe87bbf34
Henning Håkonsen <henning.hakonsen@nav.no>
2020-01-22 14:02:29 +0100

    Laget filter som gir 401 hvis client id for appen som hentet tokenet ikke er inkludert i accepted clients. (#44)

---
+++ sikkerhet/src/main/java/no/nav/familie/sikkerhet/AuthorizationFilter.kt
@@ -0,0 +1,25 @@
+package no.nav.familie.sikkerhet
+
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AuthorizationFilter(
+        private val oidcUtil: OIDCUtil,
+        private val acceptedClients: List<String>
+): OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        when (acceptedClient()) {
+            true -> filterChain.doFilter(request, response)
+            false -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authenticated, but unauthorized application")
+        }
+    }
+
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.requestURI.substring(request.contextPath.length)
+        return path.startsWith("/api/selvbetjening") || path.startsWith("/internal/")
+    }
+
+    private fun acceptedClient() = acceptedClients.contains(oidcUtil.getClaim("azp"))
+}
