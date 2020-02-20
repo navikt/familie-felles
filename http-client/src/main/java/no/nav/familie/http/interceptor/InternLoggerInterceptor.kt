5192b8e32d6212f76ae90948a543474c990eab4b
Henning Håkonsen <henninghaakonsen@me.com>
2020-02-20 15:04:39 +0100

    Endrer til preHandle for logging.

--- http-client/src/main/java/no/nav/familie/http/interceptor/InternLoggerInterceptor.kt
+++ http-client/src/main/java/no/nav/familie/http/interceptor/InternLoggerInterceptor.kt
@@ -3,21 +3,17 @@ package no.nav.familie.http.interceptor
 import no.nav.familie.http.auditlogger.AuditLogger
 import no.nav.familie.sikkerhet.OIDCUtil
 import org.slf4j.LoggerFactory
-import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class InternLoggerInterceptor(private val oidcUtil: OIDCUtil) : HandlerInterceptorAdapter() {
-    override fun postHandle(request: HttpServletRequest,
-                            response: HttpServletResponse,
-                            handler: Any,
-                            modelAndView: ModelAndView?) {
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val ansvarligSaksbehandler: String = hentSaksbehandler(oidcUtil = oidcUtil) ?: "VL"
 
         AuditLogger.logRequest(request, ansvarligSaksbehandler)
         logRequest(request, response, ansvarligSaksbehandler)
-        super.postHandle(request, response, handler, modelAndView)
+        return super.preHandle(request, response, handler)
     }
 
     companion object {
