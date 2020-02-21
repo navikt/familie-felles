1c75038f9d7dd20167998ffedea710a22ec7a942
Bent Lorentzen <bent.lorentzen@nav.no>
2020-02-21 10:19:20 +0100

    Konvertert LogFilterTest til kotlin og pyntet litt på Logfilter.

--- log/src/test/java/no/nav/familie/log/filter/LogFilterTest.kt
+++ log/src/test/java/no/nav/familie/log/filter/LogFilterTest.kt
@@ -1,101 +1,101 @@
-package no.nav.familie.log.filter;
+package no.nav.familie.log.filter
 
-import no.nav.familie.log.NavHttpHeaders;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.stubbing.Answer;
-import org.slf4j.MDC;
+import no.nav.familie.log.NavHttpHeaders
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.slf4j.MDC
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+class LogFilterTest {
 
-import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
-public class LogFilterTest {
-
-    private HttpServletRequest httpServletRequest ;
-    private HttpServletResponse httpServletResponse ;
-
-    private final LogFilter logFilter = new LogFilter();
+    private lateinit var httpServletRequest: HttpServletRequest
+    private lateinit var httpServletResponse: HttpServletResponse
+    private val logFilter = LogFilter()
 
     @Before
-    public void setup() {
-        httpServletRequest = getMockHttpServletRequest();
-        httpServletResponse = getMockHttpServletResponse();
+    fun setup() {
+        httpServletRequest = mockHttpServletRequest
+        httpServletResponse = mockHttpServletResponse
     }
 
     @Test
-    public void cleanupOfMDCContext() throws ServletException, IOException {
-        Map<String, String> initialContextMap = Optional.ofNullable(MDC.getCopyOfContextMap()).orElseGet(HashMap::new);
-        logFilter.doFilter(httpServletRequest, httpServletResponse, (request, response) -> {
-        });
-        assertThat(initialContextMap).isEqualTo(Optional.ofNullable(MDC.getCopyOfContextMap()).orElseGet(HashMap::new));
+    fun cleanupOfMDCContext() {
+        val initialContextMap =
+                MDC.getCopyOfContextMap() ?: HashMap()
+
+        logFilter.doFilter(httpServletRequest,
+                           httpServletResponse) { _, _ -> }
+
+        assertThat(initialContextMap)
+                .isEqualTo(MDC.getCopyOfContextMap() ?: HashMap<String, String>())
     }
 
     @Test
-    public void addResponseHeaders() throws ServletException, IOException {
-        logFilter.doFilter(httpServletRequest, httpServletResponse, (request, response) -> {
-        });
+    fun addResponseHeaders() {
+        logFilter.doFilter(httpServletRequest, httpServletResponse) { _, _ -> }
 
-        assertThat(httpServletResponse.getHeader(NavHttpHeaders.NAV_CALL_ID.asString())).isNotEmpty();
-        assertThat(httpServletResponse.getHeader("Server")).isNull();
+        assertThat(httpServletResponse.getHeader(NavHttpHeaders.NAV_CALL_ID.asString()))
+                .isNotEmpty()
+        assertThat(httpServletResponse.getHeader("Server")).isNull()
     }
 
     @Test
-    public void handleExceptions() throws ServletException, IOException {
-        logFilter.doFilter(httpServletRequest, httpServletResponse, (request, response) -> fail());
-        assertThat(httpServletResponse.getStatus()).isEqualTo(SC_INTERNAL_SERVER_ERROR);
+    fun handleExceptions() {
+        logFilter.doFilter(httpServletRequest, httpServletResponse) { _, _ -> fail() }
+
+        assertThat(httpServletResponse.status)
+                .isEqualTo(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
     }
 
-    private static void fail() {
-        throw new IllegalStateException("");
-    }
+    companion object {
+        private fun fail() {
+            throw IllegalStateException("")
+        }
 
+        private val mockHttpServletRequest: HttpServletRequest
+            get() {
+                val method = "GET"
+                val requestUri = "/test/path"
+                val request = Mockito.mock(HttpServletRequest::class.java)
+                Mockito.`when`(request.method).thenReturn(method)
+                Mockito.`when`(request.requestURI).thenReturn(requestUri)
+                return request
+            }
 
-    private static HttpServletRequest getMockHttpServletRequest() {
-        final String method = "GET";
-        final String requestUri = "/test/path";
-
-        var request = mock(HttpServletRequest.class);
-        when(request.getMethod()).thenReturn(method);
-        when(request.getRequestURI()).thenReturn(requestUri);
-        return request;
-    }
-
-    private static HttpServletResponse getMockHttpServletResponse() {
-
-        var response = mock(HttpServletResponse.class);
-        final Map<String, String> headers = new HashMap<>();
-        final int[] status = {0};
-
-        Answer<Void> statusAnswer = invocationOnMock -> {
-            status[0] = invocationOnMock.getArgument(0);
-            return null;
-        };
-        doAnswer(statusAnswer).when(response).setStatus(anyInt());
-
-        Answer<Void> headerAnswer = invocationOnMock -> {
-            headers.put(invocationOnMock.getArgument(0), invocationOnMock.getArgument(1));
-            return null;
-        };
-        doAnswer(headerAnswer).when(response).setHeader(anyString(), anyString());
-
-        when(response.getStatus()).thenAnswer(invocationOnMock -> status[0]);
-
-        doAnswer(invocationOnMock -> {
-            String s = invocationOnMock.getArgument(0);
-            return headers.get(s);
-        }).when(response).getHeader(anyString());
-
-        when(response.getHeaderNames()).thenAnswer(invocationOnMock -> headers.keySet());
-
-        return response;
+        private val mockHttpServletResponse: HttpServletResponse
+            get() {
+                val response =
+                        Mockito.mock(HttpServletResponse::class.java)
+                val headers: MutableMap<String, String> = HashMap()
+                val status = intArrayOf(0)
+                val statusAnswer =
+                        Answer<Void?> { invocationOnMock: InvocationOnMock ->
+                            status[0] = invocationOnMock.getArgument(0)
+                            null
+                        }
+                Mockito.doAnswer(statusAnswer).`when`(response).status = ArgumentMatchers.anyInt()
+                val headerAnswer =
+                        Answer<Void?> { invocationOnMock: InvocationOnMock ->
+                            headers[invocationOnMock.getArgument(0)] = invocationOnMock.getArgument(1)
+                            null
+                        }
+                Mockito.doAnswer(headerAnswer).`when`(response)
+                        .setHeader(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())
+                Mockito.`when`(response.status).thenAnswer { status[0] }
+                Mockito.doAnswer { invocationOnMock: InvocationOnMock ->
+                    val s = invocationOnMock.getArgument<String>(0)
+                    headers[s]
+                }.`when`(response)
+                        .getHeader(ArgumentMatchers.anyString())
+                Mockito.`when`(response.headerNames)
+                        .thenAnswer { headers.keys }
+                return response
+            }
     }
 }
