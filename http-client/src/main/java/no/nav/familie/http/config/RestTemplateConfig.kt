c8b42568012a6673e5562a9811c3963912d160e9
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-03-03 18:13:05 +0100

    Skilt ut restTemplateBuilder som egne bønner slik at man kan gjøre ytterligere tilpasning ved behov.

--- http-client/src/main/java/no/nav/familie/http/config/RestTemplateConfig.kt
+++ http-client/src/main/java/no/nav/familie/http/config/RestTemplateConfig.kt
@@ -10,39 +10,51 @@ import org.apache.http.client.HttpClient
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.impl.conn.DefaultProxyRoutePlanner
 import org.apache.http.protocol.HttpContext
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.boot.web.client.RestTemplateCustomizer
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
 
 
+@Suppress("SpringFacetCodeInspection")
 @Configuration
+@ComponentScan("no.nav.familie.http.interceptor")
 class RestTemplateConfig {
 
     @Bean("azure")
-    fun restTemplateJwtBearer(consumerIdClientInterceptor: ConsumerIdClientInterceptor,
-                              bearerTokenClientInterceptor: BearerTokenClientInterceptor): RestOperations {
+    fun restTemplateBuilderJwtBearer(consumerIdClientInterceptor: ConsumerIdClientInterceptor,
+                                     bearerTokenClientInterceptor: BearerTokenClientInterceptor): RestTemplateBuilder {
 
         return RestTemplateBuilder()
                 .additionalCustomizers(NaisProxyCustomizer())
                 .interceptors(consumerIdClientInterceptor,
                               bearerTokenClientInterceptor,
                               MdcValuesPropagatingClientInterceptor())
-                .build()
+    }
+
+    @Bean("azure")
+    fun restTemplateJwtBearer(@Qualifier("azure") restTemplateBuilder: RestTemplateBuilder): RestOperations {
+        return restTemplateBuilder.build()
     }
 
     @Bean("sts")
-    fun restTemplateSts(stsBearerTokenClientInterceptor: StsBearerTokenClientInterceptor,
-                        consumerIdClientInterceptor: ConsumerIdClientInterceptor): RestOperations {
+    fun restTemplateBuilderSts(stsBearerTokenClientInterceptor: StsBearerTokenClientInterceptor,
+                               consumerIdClientInterceptor: ConsumerIdClientInterceptor): RestTemplateBuilder? {
 
         return RestTemplateBuilder()
                 .interceptors(consumerIdClientInterceptor,
                               stsBearerTokenClientInterceptor,
                               MdcValuesPropagatingClientInterceptor())
-                .build()
+    }
+
+    @Bean("sts")
+    fun restTemplateSts(@Qualifier("sts") restTemplateBuilder: RestTemplateBuilder): RestOperations {
+        return restTemplateBuilder.build()
     }
 
 }
