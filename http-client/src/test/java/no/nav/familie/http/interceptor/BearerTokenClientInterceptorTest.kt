57640f899f02d3e37644d388b02fd6c38d33324b
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-03-04 20:17:02 +0100

    wip

--- http-client/src/test/java/no/nav/familie/http/interceptor/BearerTokenClientInterceptorTest.kt
+++ http-client/src/test/java/no/nav/familie/http/interceptor/BearerTokenClientInterceptorTest.kt
@@ -1,15 +1,23 @@
 package no.nav.familie.http.interceptor
 
 
+import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import no.nav.security.token.support.client.core.ClientAuthenticationProperties
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.OAuth2GrantType
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
+import no.nav.security.token.support.core.context.TokenValidationContext
+import no.nav.security.token.support.core.jwt.JwtTokenClaims
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.junit.Before
 import org.junit.Test
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
 import java.net.URI
 
 class BearerTokenClientInterceptorTest {
@@ -26,6 +34,23 @@ class BearerTokenClientInterceptorTest {
 
     @Test
     fun `intercept`() {
+        val tokenValidationContext = mockk<TokenValidationContext>()
+        every { tokenValidationContext.getClaims("azuread") }
+                .returns(JwtTokenClaims(JWTClaimsSet.Builder()
+                                                .issuer("azure")
+                                                .claim("preferred_username", "bob")
+                                                .build()))
+        SpringTokenValidationContextHolder().tokenValidationContext = mockk()
+        val req = mockk<HttpRequest>()
+        every { req.uri }.returns(URI("http://firstResource.no"))
+        val execution = mockk<ClientHttpRequestExecution>(relaxed = true)
+        every { execution.execute(any(), any()) }.returns(mockk())
+
+        bearerTokenClientInterceptor.intercept(req, ByteArray(0), execution)
+
+
+        verify { oAuth2AccessTokenService.getAccessToken(clientConfigurationProperties.registration["2"]) }
+
     }
 
 
@@ -37,7 +62,7 @@ class BearerTokenClientInterceptorTest {
                                                   ClientAuthenticationProperties("clientIdent",
                                                                                  ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
                                                                                  "Secrets are us",
-                                                                                 ""),
+                                                                                 null),
                                                   URI("http://firstResource.no")),
                           "2" to ClientProperties(URI("http://tokenendpoint.com"),
                                                   OAuth2GrantType.JWT_BEARER,
@@ -45,7 +70,7 @@ class BearerTokenClientInterceptorTest {
                                                   ClientAuthenticationProperties("clientIdent",
                                                                                  ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
                                                                                  "Secrets are us",
-                                                                                 ""),
+                                                                                 null),
                                                   URI("http://firstResource.no")),
                           "3" to ClientProperties(URI("http://tokenendpoint.com"),
                                                   OAuth2GrantType.CLIENT_CREDENTIALS,
@@ -53,7 +78,7 @@ class BearerTokenClientInterceptorTest {
                                                   ClientAuthenticationProperties("clientIdent",
                                                                                  ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
                                                                                  "Secrets are us",
-                                                                                 ""),
+                                                                                 null),
                                                   URI("http://secondResource.no")))
             )
 
