50c928509eda0b94b7311c7ee7e957b7da88d95e
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-06-22 12:41:14 +0200

    Logging på isLeader

--- leader/src/main/kotlin/no/nav/familie/leader/LeaderClient.kt
+++ leader/src/main/kotlin/no/nav/familie/leader/LeaderClient.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.leader
 
+import org.slf4j.LoggerFactory
 import java.net.InetAddress
 import java.net.URI
 import java.net.http.HttpClient
@@ -8,9 +9,9 @@ import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers
 
 
-
-
 object LeaderClient {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     /**
      * @return Om pod er leader eller ikke. null hvis leadersjekk ikke er implementert på pod
@@ -29,6 +30,7 @@ object LeaderClient {
         if (response.body().isNullOrBlank()) return null
 
         val hostname: String = InetAddress.getLocalHost().hostName
+        log.info("Leader hostname $hostname ${response.body()}")
         return response.body().contains(hostname)
     }
 }
