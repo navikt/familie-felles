85df694d4361dee710dfbd116b3e2ae1b6835ce2
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-09-17 14:51:36 +0200

    fjern imports

--- http-client/src/main/java/no/nav/familie/http/interceptor/BearerTokenWithSTSFallbackClientInterceptor.kt
+++ http-client/src/main/java/no/nav/familie/http/interceptor/BearerTokenWithSTSFallbackClientInterceptor.kt
@@ -1,17 +1,11 @@
 package no.nav.familie.http.interceptor
 
 import no.nav.familie.http.sts.StsRestClient
-import no.nav.security.token.support.client.core.ClientProperties
-import no.nav.security.token.support.client.core.OAuth2GrantType
-import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
-import org.springframework.beans.factory.support.SecurityContextProvider
-import org.springframework.boot.actuate.endpoint.SecurityContext
 import org.springframework.http.HttpRequest
 import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
 import org.springframework.stereotype.Component
 
