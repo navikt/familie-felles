f1299bea0650d999101b2adf07794065a9594c96
Alexandra <alexandra1.back@gmail.com>
2020-10-27 14:34:28 +0100

    Flyttet ut utregning av virkedager til felles biblioteket og lagt til bevegelige helligdager (#176)

---
+++ util/src/test/kotlin/no/nav/familie/util/VirkedagerProviderTest.kt
@@ -0,0 +1,34 @@
+package no.nav.familie.util
+
+import java.time.LocalDate
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+
+class VirkedagerProviderTest {
+    val skjærTorsdag2021 = LocalDate.of(2021, 4, 1)
+    val skjærTorsdag2022 = LocalDate.of(2022, 4, 14)
+
+    @Test
+    fun `Hent virkedag allmenlig måndag`() {
+        val allmenligMåndag = LocalDate.of(2020, 10, 26)
+        assertEquals(VirkedagerProvider.nesteVirkedag(allmenligMåndag), allmenligMåndag.plusDays(1))
+    }
+
+    @Test
+    fun `Hent virkedag allmenlig fredag`() {
+        val allmenligFredag = LocalDate.of(2020, 10, 30)
+        assertEquals(VirkedagerProvider.nesteVirkedag(allmenligFredag), allmenligFredag.plusDays(3))
+    }
+
+    @Test
+    fun `Hent virkedag skjærtorsdag 2021`() {
+        assertEquals(VirkedagerProvider.nesteVirkedag(skjærTorsdag2021), skjærTorsdag2021.plusDays(5))
+    }
+
+    @Test
+    fun `Hent virkedag skjærtorsdag 2022`() {
+        assertEquals(VirkedagerProvider.nesteVirkedag(skjærTorsdag2022), skjærTorsdag2022.plusDays(5))
+    }
+
+}
