dc154ab16b900702bb3de744d0c2166a5ecaf54d
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2021-01-13 18:03:14 +0100

    Feature/slippe authentication swagger resources (#255)
    * slipper authentication for swagger resources url
    
    * slipper authentication for api-docs url

--- sikkerhet/src/main/java/no/nav/familie/sikkerhet/AuthorizationFilter.kt
+++ sikkerhet/src/main/java/no/nav/familie/sikkerhet/AuthorizationFilter.kt
@@ -20,7 +20,10 @@ class AuthorizationFilter(
 
     override fun shouldNotFilter(request: HttpServletRequest): Boolean {
         val path = request.requestURI.substring(request.contextPath.length)
-        return path.startsWith("/api/selvbetjening") || path.startsWith("/internal/") || path.startsWith("/swagger-ui/")
+        return path.startsWith("/api/selvbetjening") || path.startsWith("/internal/")
+               || path.startsWith("/swagger-ui/") || path.startsWith("/swagger-resources/")
+               || path.startsWith("/v2/api-docs")
+
     }
 
     private fun acceptedClient() = acceptedClients.contains(oidcUtil.getClaim("azp"))
