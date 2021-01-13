12f96501379925816b329d08e6c9657e9254228a
Saikat Halder <saikat.halder@nav.no>
2021-01-13 18:55:23 +0100

    slipper authentication for api-docs url

--- sikkerhet/src/main/java/no/nav/familie/sikkerhet/AuthorizationFilter.kt
+++ sikkerhet/src/main/java/no/nav/familie/sikkerhet/AuthorizationFilter.kt
@@ -21,7 +21,7 @@ class AuthorizationFilter(
     override fun shouldNotFilter(request: HttpServletRequest): Boolean {
         val path = request.requestURI.substring(request.contextPath.length)
         return path.startsWith("/api/selvbetjening") || path.startsWith("/internal/")
-               || path.startsWith("/swagger-ui/") || path.startsWith("/swagger-resources/")
+               || path.startsWith("/swagger-ui/") || path.startsWith("/swagger-resources")
                || path.startsWith("/v2/api-docs")
 
     }
