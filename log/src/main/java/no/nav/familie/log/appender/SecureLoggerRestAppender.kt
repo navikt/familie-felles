09ed7e9dc55233475e9c88c58af297306e036b01
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-04-16 13:43:04 +0200

    Forbedringer securelogger over rest. Fikser feil hvor mdc har null verdig og lagt til generell logging ved ukjente feil

--- log/src/main/java/no/nav/familie/log/appender/SecureLoggerRestAppender.kt
+++ log/src/main/java/no/nav/familie/log/appender/SecureLoggerRestAppender.kt
@@ -14,41 +14,49 @@ class SecureLoggerRestAppender : AppenderBase<ch.qos.logback.classic.spi.ILoggin
     private val objectMapper = ObjectMapper()
 
     override fun append(eventObject: ch.qos.logback.classic.spi.ILoggingEvent) {
-        val logEvent = mutableMapOf<String, String>()
-        logEvent["message"] = eventObject.formattedMessage
-        logEvent["level"] = eventObject.level.levelStr
-        logEvent["thread"] = eventObject.threadName
-        val iThrowableProxy = eventObject.throwableProxy
+        try {
+            val logEvent = mutableMapOf<String, String>()
+            logEvent["message"] = eventObject.formattedMessage
+            logEvent["level"] = eventObject.level.levelStr
+            logEvent["thread"] = eventObject.threadName
+            val iThrowableProxy = eventObject.throwableProxy
 
-        if (iThrowableProxy != null) {
-            val stackTrace = "${iThrowableProxy.className}: ${iThrowableProxy.message}\n" +
-                    iThrowableProxy
-                        .stackTraceElementProxyArray
-                        .joinToString { "${it.steAsString}\n" }
+            if (iThrowableProxy != null) {
+                val stackTrace = "${iThrowableProxy.className}: ${iThrowableProxy.message}\n" +
+                        iThrowableProxy
+                            .stackTraceElementProxyArray
+                            .joinToString { "${it.steAsString}\n" }
 
-            logEvent["stack_trace"] = stackTrace
-        }
+                logEvent["stack_trace"] = stackTrace
+            }
 
-        val mdc = eventObject.mdcPropertyMap
-        mdc.forEach { (k, v) ->
-            logEvent[k] = v
-        }
+            val mdc = eventObject.mdcPropertyMap
+            mdc?.keys?.forEach {
+                if (!mdc[it].isNullOrEmpty()) {
+                    logEvent[it] = mdc[it]!!
+                } else {
+                    println("[securelogs] mdc key $it mangler verdi")
+                }
+            }
 
-        val request = HttpRequest.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .uri(URI.create("http://localhost:19880/"))
-            .timeout(Duration.ofSeconds(10))
-            .setHeader("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(logEvent)))
-            .build()
+            val request = HttpRequest.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .uri(URI.create("http://localhost:19880/"))
+                .timeout(Duration.ofSeconds(10))
+                .setHeader("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(logEvent)))
+                .build()
 
-        val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+            val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
 
-        if (response.statusCode() != 200) {
-            println(
-                "ERROR ved posting av melding til secureLog ${response.statusCode()} " +
-                        "${response.body()} ${response.headers()}"
-            )
+            if (response.statusCode() != 200) {
+                println(
+                    "[securelogs] ERROR ved posting av melding til secureLog ${response.statusCode()} " +
+                            "${response.body()} ${response.headers()}"
+                )
+            }
+        } catch (e: Exception) {
+            println("[securelogs] Ukjent feil ved securelogs $e")
         }
     }
 }
