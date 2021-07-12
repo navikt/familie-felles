1c8a59aa0190c359b97aab2c531504896702b8e8
Johan Blomgren <blommish@users.noreply.github.com>
2021-07-12 10:17:34 +0200

    Legger til mulighet får å hente ut ekstern bruker med tokenx

--- sikkerhet/src/main/java/no/nav/familie/sikkerhet/EksternBrukerUtils.kt
+++ sikkerhet/src/main/java/no/nav/familie/sikkerhet/EksternBrukerUtils.kt
@@ -1,11 +1,16 @@
 package no.nav.familie.sikkerhet
 
+import no.nav.familie.sikkerhet.EksternBrukerIssuer.SELVBETJENING
 import no.nav.security.token.support.core.context.TokenValidationContext
 import no.nav.security.token.support.core.exceptions.JwtTokenValidatorException
 import no.nav.security.token.support.core.jwt.JwtTokenClaims
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.springframework.web.context.request.RequestContextHolder
 
+enum class EksternBrukerIssuer(val issuer: String) {
+    SELVBETJENING(EksternBrukerUtils.ISSUER),
+    TOKEN_X("tokenx")
+}
 
 object EksternBrukerUtils {
 
@@ -13,18 +18,18 @@ object EksternBrukerUtils {
 
     private val TOKEN_VALIDATION_CONTEXT_ATTRIBUTE = SpringTokenValidationContextHolder::class.java.name
 
-    fun hentFnrFraToken(): String =
-            claims()?.subject ?: throw JwtTokenValidatorException("Fant ikke subject")
+    fun hentFnrFraToken(issuer: EksternBrukerIssuer = SELVBETJENING): String =
+            claims(issuer)?.subject ?: throw JwtTokenValidatorException("Fant ikke subject")
 
-    fun personIdentErLikInnloggetBruker(personIdent: String): Boolean =
-            personIdent == hentFnrFraToken()
+    fun personIdentErLikInnloggetBruker(personIdent: String, issuer: EksternBrukerIssuer = SELVBETJENING): Boolean =
+            personIdent == hentFnrFraToken(issuer)
 
-    fun getBearerTokenForLoggedInUser(): String =
-            getTokenValidationContext().getJwtToken(ISSUER).tokenAsString
+    fun getBearerTokenForLoggedInUser(issuer: EksternBrukerIssuer = SELVBETJENING): String =
+            getTokenValidationContext().getJwtToken(issuer.issuer).tokenAsString
 
-    private fun claims(): JwtTokenClaims? {
+    private fun claims(issuer: EksternBrukerIssuer = SELVBETJENING): JwtTokenClaims? {
         val validationContext = getTokenValidationContext()
-        return validationContext.getClaims(ISSUER)
+        return validationContext.getClaims(issuer.issuer)
     }
 
     private fun getTokenValidationContext(): TokenValidationContext {
