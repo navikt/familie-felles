5f1405d1eadc5c8722b4388c551b340a787409c4
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-08 14:19:43 +0200

    Prøver å utstede token på nytt ved feil mot eks tokendings som skjer … (#431)

---
+++ http-client/src/main/java/no/nav/familie/http/client/RetryOAuth2HttpClient.kt
@@ -0,0 +1,53 @@
+package no.nav.familie.http.client
+
+import no.nav.security.token.support.client.core.http.OAuth2HttpRequest
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
+import no.nav.security.token.support.client.spring.oauth2.DefaultOAuth2HttpClient
+import org.slf4j.LoggerFactory
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.core.NestedExceptionUtils
+import org.springframework.web.client.HttpServerErrorException
+import java.net.SocketException
+import java.net.SocketTimeoutException
+
+class RetryOAuth2HttpClient(restTemplateBuilder: RestTemplateBuilder,
+                            private val maxRetries: Int = 2) : DefaultOAuth2HttpClient(restTemplateBuilder) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+    private val retryExceptions = setOf(SocketException::class,
+                                        SocketTimeoutException::class,
+                                        HttpServerErrorException.ServiceUnavailable::class,
+                                        HttpServerErrorException.GatewayTimeout::class,
+                                        HttpServerErrorException.BadGateway::class)
+
+    override fun post(oAuth2HttpRequest: OAuth2HttpRequest): OAuth2AccessTokenResponse {
+        var retries = 0
+
+        while (true) {
+            try {
+                return super.post(oAuth2HttpRequest)
+            } catch (e: Exception) {
+                handleException(e, retries++, oAuth2HttpRequest)
+            }
+        }
+    }
+
+    private fun handleException(e: Exception,
+                                retries: Int,
+                                oAuth2HttpRequest: OAuth2HttpRequest) {
+        if (shouldRetry(e) && retries < maxRetries) {
+            logger.warn("Kall mot url=${oAuth2HttpRequest.tokenEndpointUrl} feilet, cause=${
+                NestedExceptionUtils.getMostSpecificCause(e)::class
+            }")
+            secureLogger.warn("Kall mot url=${oAuth2HttpRequest.tokenEndpointUrl} feilet med feil=${e.message}")
+        } else {
+            throw e
+        }
+    }
+
+    private fun shouldRetry(e: Exception): Boolean {
+        return retryExceptions.contains(e.cause?.let { it::class })
+    }
+}
---
+++ http-client/src/test/java/no/nav/familie/http/client/RetryOAuth2HttpClientTest.kt
@@ -0,0 +1,111 @@
+package no.nav.familie.http.client
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.http.Fault
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
+import no.nav.security.token.support.client.core.http.OAuth2HttpHeaders
+import no.nav.security.token.support.client.core.http.OAuth2HttpRequest
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.web.client.RestTemplateBuilder
+import java.net.URI
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+
+internal class RetryOAuth2HttpClientTest {
+
+    private val restTemplateBuilder = RestTemplateBuilder()
+            .setConnectTimeout(Duration.of(1, ChronoUnit.SECONDS))
+            .setReadTimeout(Duration.of(1, ChronoUnit.SECONDS))
+
+    private val client = RetryOAuth2HttpClient(restTemplateBuilder)
+
+    @BeforeEach
+    internal fun setUp() {
+        wireMockServer.resetAll()
+    }
+
+    @Test
+    internal fun `200 - skal kun kalle en gang`() {
+        stub(WireMock.aResponse().withBody("{}"))
+        post()
+        wireMockServer.verify(1, RequestPatternBuilder.allRequests())
+    }
+
+    @Test
+    internal fun `404 - skal kun kalle en gang`() {
+        stub(WireMock.serverError().withStatus(404))
+        post()
+        wireMockServer.verify(1, RequestPatternBuilder.allRequests())
+    }
+
+    @Test
+    internal fun `503 - skal prøve på nytt`() {
+        stub(WireMock.aResponse().withStatus(503))
+        post()
+        wireMockServer.verify(3, RequestPatternBuilder.allRequests())
+    }
+
+    @Test
+    internal fun `502 - skal prøve på nytt`() {
+        stub(WireMock.serverError().withStatus(502))
+        post()
+        wireMockServer.verify(3, RequestPatternBuilder.allRequests())
+    }
+
+    @Test
+    internal fun `socketException - skal prøve på nytt`() {
+        stub(WireMock.serverError().withFault(Fault.CONNECTION_RESET_BY_PEER))
+        post()
+        wireMockServer.verify(3, RequestPatternBuilder.allRequests())
+    }
+
+    @Test
+    internal fun `timeout - skal prøve på nytt ved timeout`() {
+        stub(WireMock.serverError().withFixedDelay(2000))
+        post()
+        wireMockServer.verify(3, RequestPatternBuilder.allRequests())
+    }
+
+    private fun stub(responseDefinitionBuilder: ResponseDefinitionBuilder?) {
+        wireMockServer.stubFor(WireMock.post(WireMock.anyUrl())
+                                       .willReturn(responseDefinitionBuilder))
+    }
+
+    private fun post(): Exception? {
+        return try {
+            client.post(OAuth2HttpRequest.builder()
+                                .tokenEndpointUrl(URI.create(wireMockServer.baseUrl()))
+                                .oAuth2HttpHeaders(OAuth2HttpHeaders.builder().build())
+                                .build())
+            null
+        } catch (e: Exception) {
+            e
+        }
+    }
+
+    companion object {
+
+        private lateinit var wireMockServer: WireMockServer
+
+        @BeforeAll
+        @JvmStatic
+        fun initClass() {
+            wireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+            wireMockServer.start()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            wireMockServer.stop()
+        }
+    }
+
+
+}
