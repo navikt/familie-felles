3d761e00310ae494564df80c6912b88c4a5a8edd
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-09 16:13:44 +0200

    Lagt til Nav-User-Id header som skal brukes mot integrasjoner og mot journalføring

--- log/src/main/java/no/nav/familie/log/NavHttpHeaders.kt
+++ log/src/main/java/no/nav/familie/log/NavHttpHeaders.kt
@@ -1,7 +1,10 @@
 package no.nav.familie.log
 
 enum class NavHttpHeaders(private val header: String) {
-    NAV_PERSONIDENT("Nav-Personident"), NAV_CALL_ID("Nav-Call-Id"), NAV_CONSUMER_ID("Nav-Consumer-Id");
+    NAV_PERSONIDENT("Nav-Personident"),
+    NAV_CALL_ID("Nav-Call-Id"),
+    NAV_CONSUMER_ID("Nav-Consumer-Id"),
+    NAV_USER_ID("Nav-User-Id");
 
     fun asString(): String {
         return header
