7ede38875f86cb0defbda69e93c431a52284c8cf
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-09-21 14:45:14 +0200

    Proxy forutsetter nå at det er definert en proxyurl i config.

--- http-client/src/main/java/no/nav/familie/http/config/RestTemplateBuilderBean.kt
+++ http-client/src/main/java/no/nav/familie/http/config/RestTemplateBuilderBean.kt
@@ -1,9 +1,11 @@
 package no.nav.familie.http.config
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
 
 @Suppress("SpringFacetCodeInspection")
 @Configuration
@@ -11,8 +13,15 @@ import org.springframework.context.annotation.Import
 class RestTemplateBuilderBean {
 
     @Bean
+    @Primary
+    @ConditionalOnProperty("no.nav.security.jwt.issuer.azuread.proxyurl")
     fun restTemplateBuilder(iNaisProxyCustomizer: INaisProxyCustomizer): RestTemplateBuilder {
         return RestTemplateBuilder(iNaisProxyCustomizer)
     }
-}
 
+    @Bean
+    fun restTemplateBuilderNoProxy(): RestTemplateBuilder {
+        return RestTemplateBuilder()
+    }
+
+}
