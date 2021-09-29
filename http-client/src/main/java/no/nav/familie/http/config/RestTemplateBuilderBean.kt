040b3e7b0d9fb29fe1f2cc4d572cae2d101d985b
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2021-09-29 09:04:40 +0200

    Proxy forutsetter nå at det er definert en proxyurl i config. (#449)

--- http-client/src/main/java/no/nav/familie/http/config/RestTemplateBuilderBean.kt
+++ http-client/src/main/java/no/nav/familie/http/config/RestTemplateBuilderBean.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.http.config
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,8 +12,19 @@ import org.springframework.context.annotation.Import
 class RestTemplateBuilderBean {
 
     @Bean
+    @ConditionalOnProperty("no.nav.security.jwt.issuer.azuread.proxyurl")
     fun restTemplateBuilder(iNaisProxyCustomizer: INaisProxyCustomizer): RestTemplateBuilder {
         return RestTemplateBuilder(iNaisProxyCustomizer)
     }
-}
 
+    /**
+     * Denne bønnnen initialiseres hvis proxy-url ikke finnes. Hvis proxy-url finnnes vil bønnen over initialiseres og
+     * denne det ikke med mindre proxyen har verdien "umulig verdi", som den aldri skal ha.
+     */
+    @Bean
+    @ConditionalOnProperty("no.nav.security.jwt.issuer.azuread.proxyurl", matchIfMissing = true, havingValue = "Umulig verdi")
+    fun restTemplateBuilderNoProxy(): RestTemplateBuilder {
+        return RestTemplateBuilder()
+    }
+
+}
