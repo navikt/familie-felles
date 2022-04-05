6df4d705c4490c9444eb555034be8510772aa230
Meng Zhu <meng.zhu@nav.no>
2022-04-05 12:09:53 +0200

    Remove Prosessering (#524)
    * remove prosessering
    
    * remove dependencies

--- prosessering/src/main/java/no/nav/familie/prosessering/internal/RekjørSenereException.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.prosessering.internal
-
-import java.time.LocalDateTime
-
-data class RekjørSenereException(val årsak: String, val triggerTid: LocalDateTime): RuntimeException()
