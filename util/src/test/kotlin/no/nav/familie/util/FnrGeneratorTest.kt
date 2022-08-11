cc7908783813527417478120b94ee0b9d5379d45
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-08-11 10:41:29 +0200

    Flyttet Periode og Fødselsnummer til kontrakter, siden disse også fungerer som DTOer.

--- util/src/test/kotlin/no/nav/familie/util/FnrGeneratorTest.kt
+++ util/src/test/kotlin/no/nav/familie/util/FnrGeneratorTest.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.util
 
-import no.nav.familie.Fødselsnummer
+import no.nav.familie.kontrakter.ef.søknad.Fødselsnummer
 import org.junit.jupiter.api.Test
 
 class FnrGeneratorTest {
