0e6e0798d513e61b698fb9b91df823d1accd3d41
bragejahren <brage.ekroll.jahren@nav.no>
2022-08-30 09:08:18 +0200

    Fjernet @Component fra ECBRestClientInterceptor

--- http-client/src/main/java/no/nav/familie/http/ecb/config/ECBRestClientConfig.kt
+++ http-client/src/main/java/no/nav/familie/http/ecb/config/ECBRestClientConfig.kt
@@ -8,14 +8,12 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestOperations
 
 @Suppress("SpringFacetCodeInspection")
 @Configuration
-@Import(ECBRestClientInterceptor::class)
 class ECBRestClientConfig {
 
     @Bean("ecbMapper")
@@ -28,13 +26,13 @@ class ECBRestClientConfig {
     }
 
     @Bean("ecbRestTemplate")
-    fun xmlRestTemplate(ecbRestClientInterceptor: ECBRestClientInterceptor, @Qualifier("ecbMapper") xmlMapper: XmlMapper): RestOperations {
+    fun xmlRestTemplate(@Qualifier("ecbMapper") xmlMapper: XmlMapper): RestOperations {
         val converter = MappingJackson2HttpMessageConverter(xmlMapper).apply {
             supportedMediaTypes = listOf(MediaType.parseMediaType("application/vnd.sdmx.genericdata+xml;version=2.1"))
         }
         return RestTemplateBuilder()
             .additionalMessageConverters(converter)
-            .additionalInterceptors(ecbRestClientInterceptor)
+            .additionalInterceptors(ECBRestClientInterceptor())
             .build()
     }
 }
