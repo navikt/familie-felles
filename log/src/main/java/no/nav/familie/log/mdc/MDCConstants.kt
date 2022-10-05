87249021b1241c4ef0bd201fe17ea4ad4a0ee1c3
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-10-05 14:50:11 +0200

    Skal ha underscore i request_id for å matche nginx - da kansden plukkes opp av nginx dersom vi har satt den først

--- log/src/main/java/no/nav/familie/log/mdc/MDCConstants.kt
+++ log/src/main/java/no/nav/familie/log/mdc/MDCConstants.kt
@@ -4,5 +4,9 @@ object MDCConstants {
     const val MDC_USER_ID = "userId"
     const val MDC_CONSUMER_ID = "consumerId"
     const val MDC_CALL_ID = "callId"
-    const val MDC_REQUEST_ID = "requestId"
+
+    /**
+     * trengs for å spore kall som går via nginx/controller - den bruker underscore
+     **/
+    const val MDC_REQUEST_ID = "request_Id"
 }
