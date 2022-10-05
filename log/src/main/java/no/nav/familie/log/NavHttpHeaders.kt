de5e326b5c0a0bbb48615287c958ce425524ec25
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-10-05 15:46:26 +0200

    Tror riktig utgående request-id til nginx må være X-Request-Id

--- log/src/main/java/no/nav/familie/log/NavHttpHeaders.kt
+++ log/src/main/java/no/nav/familie/log/NavHttpHeaders.kt
@@ -3,7 +3,7 @@ package no.nav.familie.log
 enum class NavHttpHeaders(private val header: String) {
     NAV_PERSONIDENT("Nav-Personident"),
     NAV_CALL_ID("Nav-Call-Id"),
-    NGNINX__REQUEST_ID("X_Request_Id"),
+    NGNINX_REQUEST_ID("X-Request-Id"),
     NAV_CONSUMER_ID("Nav-Consumer-Id"),
     NAV_USER_ID("Nav-User-Id");
 
