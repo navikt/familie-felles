2be5b5d68d0911206349659f818418b7807cbc8b
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-10-05 16:10:24 +0200

    Reverter endring av mdc_request_id-verdi pga misforståelse.

--- log/src/main/java/no/nav/familie/log/mdc/MDCConstants.kt
+++ log/src/main/java/no/nav/familie/log/mdc/MDCConstants.kt
@@ -4,9 +4,5 @@ object MDCConstants {
     const val MDC_USER_ID = "userId"
     const val MDC_CONSUMER_ID = "consumerId"
     const val MDC_CALL_ID = "callId"
-
-    /**
-     * trengs for å spore kall som går via nginx/controller - den bruker underscore
-     **/
-    const val MDC_REQUEST_ID = "request_Id"
+    const val MDC_REQUEST_ID = "requestId"
 }
