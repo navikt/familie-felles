e57764f208330fee5047a27070682185be3cac5e
Johan Blomgren <blommish@users.noreply.github.com>
2022-10-20 18:51:32 +0200

    Fikset test

--- web-client/src/test/java/no/nav/familie/webflux/builder/WebClientMetricConfig.kt
+++ web-client/src/test/java/no/nav/familie/webflux/builder/WebClientMetricConfig.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.webflux.builder
 
-import io.mockk.mockk
+import io.micrometer.core.instrument.Tag
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import org.springframework.boot.actuate.metrics.AutoTimer
 import org.springframework.boot.actuate.metrics.web.reactive.client.MetricsWebClientCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,5 +11,10 @@ import org.springframework.context.annotation.Configuration
 class WebClientMetricConfig {
 
     @Bean
-    fun metricsWebClientCustomizer() = MetricsWebClientCustomizer(mockk(), mockk(), "", mockk())
+    fun metricsWebClientCustomizer() = MetricsWebClientCustomizer(
+        SimpleMeterRegistry(),
+        { _, _, _ -> emptyList<Tag>() },
+        "",
+        AutoTimer.ENABLED
+    )
 }
