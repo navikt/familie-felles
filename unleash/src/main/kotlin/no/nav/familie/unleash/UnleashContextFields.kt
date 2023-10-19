0a3df34cecd855eed8610d3697528c4163b492b9
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-10-19 12:33:22 +0200

    Feat/legg til ident og epost unleash som contextfelter (#736)
    * Legg til navIdent og epost som kontekstfelter
    
    * Gjør UnleashContextFields til const

--- unleash/src/main/kotlin/no/nav/familie/unleash/UnleashContextFields.kt
+++ unleash/src/main/kotlin/no/nav/familie/unleash/UnleashContextFields.kt
@@ -3,6 +3,8 @@ package no.nav.familie.unleash
 // Feltene må også være definert i unleash. Gjøres via Configure -> Context fields
 // https://teamfamilie-unleash-web.nav.cloud.nais.io/context
 object UnleashContextFields {
-    val FAGSAK_ID = "fagsakId"
-    val ENHET_ID = "enhetId"
+    const val FAGSAK_ID = "fagsakId"
+    const val ENHET_ID = "enhetId"
+    const val NAV_IDENT = "navIdent"
+    const val EPOST = "epost"
 }
