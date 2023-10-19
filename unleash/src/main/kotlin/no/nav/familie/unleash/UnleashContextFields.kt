ef924577400c55eb2ff13076f066a7965432b355
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-10-19 11:40:30 +0200

    Legg til enhetId som context field (#734)
    * Legg til enhet som context field
    
    * Enhet -> enhetId

--- unleash/src/main/kotlin/no/nav/familie/unleash/UnleashContextFields.kt
+++ unleash/src/main/kotlin/no/nav/familie/unleash/UnleashContextFields.kt
@@ -1,5 +1,8 @@
 package no.nav.familie.unleash
 
+// Feltene må også være definert i unleash. Gjøres via Configure -> Context fields
+// https://teamfamilie-unleash-web.nav.cloud.nais.io/context
 object UnleashContextFields {
     val FAGSAK_ID = "fagsakId"
+    val ENHET_ID = "enhetId"
 }
