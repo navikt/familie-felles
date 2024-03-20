a7fa66bac20f830561f3582a1227c0b5fd4c673d
halvor <halvorbmundal@gmail.com>
2024-03-20 13:50:21 +0100

    ValutakursFeilException -> ValutakursException

--- valutakurs-klient/src/main/java/no/nav/familie/valutakurs/exception/ValutakursClientException.kt
+++ valutakurs-klient/src/main/java/no/nav/familie/valutakurs/exception/ValutakursClientException.kt
@@ -2,6 +2,6 @@ package no.nav.familie.valutakurs.exception
 
 sealed class ValutakursClientException(override val message: String, override val cause: Throwable?) : RuntimeException(message, cause)
 
-class ValutakursFeilException(override val message: String, override val cause: Throwable?) : ValutakursClientException(message, cause)
+class ValutakursException(override val message: String, override val cause: Throwable?) : ValutakursClientException(message, cause)
 
 class IngenValutakursException(override val message: String, override val cause: Throwable?) : ValutakursClientException(message, cause)
