f013b49f6e2b0f9e4062e11d667ebe5df0c950e6
Stig Strøm <stig.helge.strom@nav.no>
2024-05-14 14:37:11 +0200

    Muligheter til å ekskludere endepunkter i metrikker i config og rename av configverdi (#802)

--- metrikker/src/main/java/no/nav/familie/metrikker/TellAPIEndpunkterIBrukWebConfig.kt
+++ metrikker/src/main/java/no/nav/familie/metrikker/TellAPIEndpunkterIBrukWebConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@ConditionalOnProperty("familie.tellAPIEndepunkterIBruk")
+@ConditionalOnProperty("familie.tellAPIEndepunkterIBruk.enabled")
 open class TellAPIEndpunkterIBrukWebConfig : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(TellAPIEndepunkterIBrukInterceptor()).addPathPatterns("/api/**")
