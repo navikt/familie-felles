776c72fe46e1c60203bee75bf76aadae41595848
Stig Strøm <stig.helge.strom@nav.no>
2025-02-20 21:55:13 +0100

    Lagt til retry med delay på reader for å minske antall alarmer når sidecaren går ned før appen (#860)

--- leader/src/main/kotlin/no/nav/familie/leader/Environment.kt
+++ leader/src/main/kotlin/no/nav/familie/leader/Environment.kt
@@ -3,6 +3,6 @@ package no.nav.familie.leader
 object Environment {
     @JvmStatic
     fun hentLeaderSystemEnv(): String? {
-        return System.getenv("ELECTOR_PATH") ?: return null
+        return System.getenv("ELECTOR_GET_URL") ?: return null
     }
 }
