b2454d6f58d2d6448c3e72645998e000aa13688a
olekvernberg <ole.christian.kvernberg@nav.no>
2025-11-25 13:25:38 +0100

    NaisProxyCustomizer må arve fra RestTemplateCustomizer

--- http-client/src/main/java/no/nav/familie/http/config/NaisProxyCustomizer.kt
+++ http-client/src/main/java/no/nav/familie/http/config/NaisProxyCustomizer.kt
@@ -11,20 +11,20 @@ import org.apache.hc.core5.http.io.SocketConfig
 import org.apache.hc.core5.http.protocol.HttpContext
 import org.apache.hc.core5.util.Timeout
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.restclient.RestTemplateBuilder
-import org.springframework.context.annotation.Bean
+import org.springframework.boot.restclient.RestTemplateCustomizer
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
-import java.time.Duration
+
+interface INaisProxyCustomizer : RestTemplateCustomizer
 
 @Component
 class NaisProxyCustomizer(
     @Value("\${familie.nais.proxy.connectTimeout:15000}") val connectTimeout: Long,
     @Value("\${familie.nais.proxy.socketTimeout:15000}") val socketTimeout: Long,
     @Value("\${familie.nais.proxy.requestTimeout:15000}") val requestTimeout: Long,
-) {
-    fun customize(builder: RestTemplateBuilder): RestTemplateBuilder {
+) : INaisProxyCustomizer {
+    override fun customize(restTemplate: RestTemplate) {
         val proxy = HttpHost("webproxy-nais.nav.no", 8088)
 
         val connectionConfig =
@@ -61,12 +61,6 @@ class NaisProxyCustomizer(
                     },
                 ).build()
 
-        return builder.requestFactory { HttpComponentsClientHttpRequestFactory(client) }
+        restTemplate.requestFactory = HttpComponentsClientHttpRequestFactory(client)
     }
-
-    @Bean
-    fun restTemplate(
-        builder: RestTemplateBuilder,
-        naisProxyCustomizer: NaisProxyCustomizer,
-    ): RestTemplate = naisProxyCustomizer.customize(builder).build()
 }
